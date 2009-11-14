ac4e1ccee7103c5e7b0a6c2f838373c851b95941
Danny Alvarez <danny@fooala.com>
2009-11-14 14:21:06 -0500

    These are some basic adds to the index page, removing the default rails page, adding in a user model and migration.

--- db/migrate/20091018163815_create_users.rb
+++ db/migrate/20091018163815_create_users.rb
@@ -3,8 +3,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :fname
       t.string :lname
-      t.string :username
-      t.string :encryptedPassword
+      t.string :andrewID
+      t.boolean :admin?, :default => false
+      t.timestamp :last_login
+      t.string :email
       t.string :year
       t.string :college
 
