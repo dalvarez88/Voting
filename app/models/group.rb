10b4533fb70b2482fde94beb3075fb9551a9f1ab
keying <kbi@andrew.cmu.edu>
2009-12-02 19:46:41 -0500

    add vote page

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,5 @@ class Group < ActiveRecord::Base
   belongs_to :gadmin
   
   validates_presence_of :gname
-  validates_numericality_of :public
+  validates_numericality_of :public, :with=>/[01]/, :message =>"must be 0 or 1"
 end
